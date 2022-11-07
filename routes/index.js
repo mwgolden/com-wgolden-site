9d6e6bfaf9ec344d25de53b34e2fa356add43d25
Matthew Golden <matthew@wgolden.com>
2022-11-07 08:40:24 -0600

    paritals for bootstrap cards

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var request = require('request');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, response, next) {
+  request('http://localhost:9090/posts', {json: true},
+    (err, res, body) => {
+      if(err) { return console.log(err)}
+      console.log(body.url)
+      console.log(body)
+      response.render('index', { title: 'Express', "posts": body });
+    }
+  )
+  
 });
 
 module.exports = router;
